526b9348020c37ea221ed2353d7988e1a2c1f6f5
Julien Laurenceau <julienlaurenceau@gmail.com>
2024-09-05 18:49:39 +0200

    Update minioDiscover.sh

--- scripts/minioDiscover.sh
+++ scripts/minioDiscover.sh
@@ -39,6 +39,7 @@ echo '-----------------------------'
 echo "===== System ====="
 hostname; uname -a; uptime; echo 'Boot time :'; date -d @$(vmstat --stats | awk '/boot time/ {print $1}'); systemctl list-units --no-pager
 echo "===== CPU ====="
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 lscpu
 echo "===== MEM ====="
 free -m
