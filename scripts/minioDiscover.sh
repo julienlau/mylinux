04b7ef677823bae5f8b1ecb9e4c6b3ea1844a0e0
Julien Laurenceau <julienlaurenceau@gmail.com>
2024-09-16 11:22:08 +0200

    Update minioDiscover.sh

--- scripts/minioDiscover.sh
+++ scripts/minioDiscover.sh
@@ -49,7 +49,7 @@ free -m
 lsmem
 echo "===== Disk ====="
 cat /etc/fstab
-lsblk -o NAME,KNAME,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,MOUNTPOINT
+lsblk -o NAME,KNAME,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,LABEL,MOUNTPOINT
 lshw -class disk
 # TODO : adapt to conf
 smartctl -a /dev/sd*
